7c31eebfa15a5dd53ce6173aa0a71dfaa5263117
nullzl <nullzl@126.com>
2021-03-18 15:45:37 +0800

    * LeetCode513 二叉树最底层最左节点 - 遍历二叉树，使用left记录最左，depth记录最左的深度 - 对于每个节点，如果深度大于depth，则更新left

---
+++ src/main/java/com/nullzl/leetcode600/LeetCode513.java
@@ -0,0 +1,26 @@
+package com.nullzl.leetcode600;
+
+import com.nullzl.util.TreeNode;
+
+public class LeetCode513 {
+
+    int depth = Integer.MIN_VALUE;
+    int val = -1;
+
+    private void dfs(TreeNode root,int d){
+        if(null == root)
+            return ;
+        dfs(root.left,d + 1);
+        dfs(root.right,d + 1);
+        if(d > depth) {
+            depth = d;
+            val = root.val;
+        }
+
+    }
+
+    public int findBottomLeftValue(TreeNode root) {
+        dfs(root,0);
+        return val;
+    }
+}
